8dad563468c8ea45ee74b6aff69dfc50c50725e6
root <pavankalyan141640@gmail.com>
2020-12-12 19:55:18 +0530

    Calcualtor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+
+  input:string="1+6+3+5";
+  output:string="15";
+
+  evaluate(){
+    console.log(this.input);
+    
+    this.output=eval(this.input);
+  }
+
+  addToInput(literal){
+    if(this.input.length>50)
+    {
+      this.input='';
+      this.output='';
+      alert("Hello this is normal calculator. Not a super Computer")
+      return;
+    }
+    if(literal === 'AC')
+    {
+      this.input='';
+      this.output='';
+      return
+    }
+    if(literal === 'BS')
+    {
+      this.input=this.input.substring(0,this.input.length-1)
+      return;
+    }
+    if(literal === 'x')
+    {
+      this.input+='x';
+      return;
+    }
+
+
+    this.input+=literal;
+    let exp=this.input.replace('x','*')
+    this.output=eval(exp)
+  }
+
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatRippleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
