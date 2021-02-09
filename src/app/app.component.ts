da5c9ce5555f706907dc066411259d08447415c2
pavankalyan.c <pavankalyan141640@gmail.com>
2021-02-09 13:13:56 +0530

    fixed few bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,52 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Calculator';
 
-  input:string="1+6+3+5";
-  output:string="15";
+  input: string = '';
+  output: string = '';
 
-  evaluate(){
+  evaluate() {
     console.log(this.input);
-    
-    this.output=eval(this.input);
+
+    this.setOutput();
   }
 
-  addToInput(literal){
-    if(this.input.length>50)
-    {
-      this.input='';
-      this.output='';
-      alert("Hello this is normal calculator. Not a super Computer")
+  addToInput(literal) {
+    if (this.input.length > 50) {
+      this.input = '';
+      this.output = '';
+      alert('Hello this is normal calculator. Not a super Computer');
       return;
     }
-    if(literal === 'AC')
-    {
-      this.input='';
-      this.output='';
-      return
-    }
-    if(literal === 'BS')
-    {
-      this.input=this.input.substring(0,this.input.length-1)
+    if (literal === 'AC') {
+      this.input = '';
+      this.output = '';
       return;
     }
-    if(literal === 'x')
-    {
-      this.input+='x';
+    if (literal === 'BS') {
+      this.input = this.input.substring(0, this.input.length - 1);
+      try {
+        this.setOutput();
+      } catch (error) {
+        console.log(error);
+      }
+      return;
+    }
+    if (literal === 'x') {
+      this.input += 'x';
       return;
     }
 
-
-    this.input+=literal;
-    let exp=this.input.replace('x','*')
-    this.output=eval(exp)
+    this.input += literal;
+    this.setOutput();
   }
 
-  
+  setOutput() {
+    let exp = this.input.split('x').join('*');
+    console.log(exp);
+
+    try {
+      this.output = eval(exp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
-
-
